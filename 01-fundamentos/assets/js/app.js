8490a8401480356f0dd78123ad103a91d83c93de
David Lopez Sotelo <lopezsotelo77@gmail.com>
2026-07-02 14:12:58 +0200

    initial commit

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,48 @@
+
+
+//console.log('Hola mundo')
+
+
+// variables
+let l = 10// se usa cuando el valor puede cambiar;   a = 20
+
+const c=20// Se usa cuando el valor no debería reasignarse; c=30 error
+
+var v = 20// en deshuso; El problema principal es que var no respeta bien el alcance de bloque.
+
+/*
+Diferencia rápida
+Declaración 	Se puede reasignar  	Se puede redeclarar 	Alcance
+let	                    Sí	                No	                Bloque { }
+const	                No	                No              	Bloque { }
+var	                    Sí              	Sí	                Función / global
+*/
+
+
+
+// multiples variables
+
+    let a=10,
+        b=20, 
+        x=30;
+
+
+// comandos de consola      
+//console.log(a,b,c)
+
+//console.error('Error')
+//console.warn('Advertencia')
+//console.info('Información')
+//console.table({a,b,c})
+
+
+//Depuracion
+
+//console.assert(1===2,'No son iguales')
+
+console.clear()
+
+// mas sobre console y depuracion en navegador en: video 13:https://www.udemy.com/course/javascript-fernando-herrera/learn/lecture/17186080#overview
+
+
+var miNombre='David'
