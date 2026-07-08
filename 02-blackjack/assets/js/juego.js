a250f880d92e13b550bdcc1a49a06b2ca115e9ae
David Lopez Sotelo <lopezsotelo77@gmail.com>
2026-07-08 11:28:35 +0200

    turnos ok

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -22,6 +22,9 @@ const btnNuevo = document.querySelector('#nuevo-juego');
 let puntosJugadorHTML=document.querySelector('#puntos-jugador');
 let puntosComputadoraHTML=document.querySelector('#puntos-computadora');
 
+const divCartasJugador=document.querySelector('#Jugador-cartas');
+const divCartasComputadora=document.querySelector('#Computador-cartas');
+
 
 
 
@@ -61,19 +64,14 @@ const tomarCarta = () => {
     const carta = deck.pop(); // pop() elimina el ultimo elemento de un array y lo retorna, en este caso la ultima carta del deck.
     
     
-    //console.log(deck);
-    //console.log(carta);
-    
     if(!carta){
         throw 'No hay cartas en el deck';// throw lanza un error, en este caso si no hay cartas en el deck.
         console.warn('No hay cartas en el deck');// console.warn() muestra un mensaje de advertencia en la consola, en este caso si no hay cartas en el deck.
     }
     return carta;
 }
-/* 
-for (let i=0; i<60; i++){  // funcion que vacia la baraja de cartas, para probar que la funcion tomarCarta() funciona correctamente.
-tomarCarta();
-} */
+
+
 
 // Funcion para obtener el valor de la carta
 const valorCarta = (carta) => {
@@ -86,50 +84,134 @@ const valorCarta = (carta) => {
  }
 //console.log(valorCarta(tomarCarta())); // llama a la funcion tomarCarta() y le pasa el valor de la carta a la funcion valorCarta() para obtener el valor de la carta.
 
- 
+
+// turno computadora-------------forma del profesor-------------------------------------------------
+/* const turnoComputadora = (puntosJugador) => {
+
+    do {
+        puntosComputadora = puntosComputadora + valorCarta(carta);  // suma el valor de la carta al puntaje de la computadora
+        puntosComputadoraHTML.innerText = puntosComputadora; // actualiza el puntaje de la computadora en el HTML
+        console.log('puntos computadora = ' + puntosComputadora);
+        const imgCarta = document.createElement('img'); // crea un elemento img para mostrar la carta en el HTML
+        imgCarta.src = `02-blackjack/assets/cartas/${carta}.png`;// asigna la ruta de la imagen de la carta al elemento img
+        imgCarta.classList.add('carta'); // agrega la clase carta al elemento img para darle estilo
+        divCartasComputadora.appendChild(imgCarta); // agrega el elemento img al div de cartas de la computadora para mostrar la carta en el HTML
+       
+
+    }while(puntosComputadora<puntosJugador && puntosComputadora<=21){
+
+    };
+} */
+//------------------------------------------------------------------------------------------------
+
+
+const turno =(carta, puntosActuales, puntosHTML, divCartas, nombre)=>{
+
+     puntosActuales = puntosActuales + valorCarta(carta);
+    puntosHTML.innerText = puntosActuales;
+
+       console.log(`puntos ${nombre} = ${puntosActuales}`);
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `02-blackjack/assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+    divCartas.appendChild(imgCarta);
+
+    return puntosActuales;
+
+}
+
+
+
+
 
 // eventos
 btnPedir.addEventListener('click', () => { // evento que se ejecuta cuando se hace click en el boton pedir carta
-    const carta = tomarCarta();
+    const carta = tomarCarta(); // llama a la funcion tomarCarta() y le asigna el valor de la carta a la variable carta
 
-    if (turnoJugador){
-    puntosJugador = puntosJugador + valorCarta(carta); // suma el valor de la carta al puntaje del jugador
-    puntosJugadorHTML.innerText = puntosJugador; // actualiza el puntaje del jugador en el HTML
-    console.log('puntos jugador = ' + puntosJugador);
 
-    if (puntosJugador > 21){
-    
-        console.warn('Lo siento mucho, perdiste');  
-        turnoJugador = false; // cambia el turno del jugador a falso para que no pueda seguir pidiendo cartas   
-    }
-    }else{
-        puntosComputadora = puntosComputadora + valorCarta(carta);  // suma el valor de la carta al puntaje de la computadora
-        
-        puntosComputadoraHTML.innerText = puntosComputadora; // actualiza el puntaje de la computadora en el HTML
-        console.log('puntos computadora = ' + puntosComputadora);
-        if (puntosComputadora > 21){
-            nuevoJuego();
-            console.warn('lo siento mucho, perdiste la computadora');  
+    if(turnoJugador){
+        puntosJugador=turno(
+            carta,
+            puntosJugador,
+            puntosJugadorHTML,
+            divCartasJugador,
+            'jugador'
+        );
+
+ if (puntosJugador > 21) {
+            console.warn('Lo siento mucho, perdiste');
+            turnoJugador = false;
+        }
+
+    } else {
+        puntosComputadora =turno(
+             carta,
+            puntosComputadora,
+            puntosComputadoraHTML,
+            divCartasComputadora,
+            'computadora'
+        );
+
+
+        if (puntosComputadora > 21) {
+            
+            console.warn('lo siento mucho, perdiste la computadora');
+        } else if (puntosComputadora === 21) {
+            
+            console.warn('Felicidades, ganaste la computadora');
+        } else if (puntosComputadora > puntosJugador) {
+            
+            console.warn('lo siento mucho, perdiste la computadora');
+        } else if (puntosComputadora === puntosJugador) {
+            
+            console.warn('Empate');
         }
     }
 });
 
+
 btnDetener.addEventListener('click', () => {
-    turnoJugador = false;   
+    if (!turnoJugador){
+ finPartida(); 
+    }
+    turnoJugador = false;  
     });
+
+const finPartida=()=>{
+    if(!turnoJugador && puntosComputadora>puntosJugador){
+        console.log('gana computadora.')
+            // deshabilitar boton pedir carta
+btnPedir.disabled = true;
+    }else if(puntosComputadora==puntosJugador){
+        console.log('Habeis empatado');
+            // deshabilitar boton pedir carta
+btnPedir.disabled = true;
+    }else {console.log('gana jugador');
+
+    // deshabilitar boton pedir carta
+btnPedir.disabled = true;
+}
+}
 
 btnNuevo.addEventListener('click', () => {
     nuevoJuego();
 });     
 
 const nuevoJuego = () => {
+    btnPedir.disabled=false;
     console.log('Nuevo juego');
-        puntosJugador =0;
+    puntosJugador =0;
     puntosComputadora =0;
     turnoJugador = true;
     deck = []; // vacia el deck de cartas
     deck = crearDeck(); // crea un nuevo deck de cartas
+
     puntosJugadorHTML.innerText = puntosJugador;
     puntosComputadoraHTML.innerText = puntosComputadora;
+
+    // borrar las cartas
+    divCartasJugador.replaceChildren();
+    divCartasComputadora.replaceChildren();
 
 }
