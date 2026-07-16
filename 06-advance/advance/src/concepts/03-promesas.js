26fbdc4b59cae8c48a9ac8c8003897314d387575
David Lopez Sotelo <lopezsotelo77@gmail.com>
2026-07-16 10:22:07 +0200

    add promise

--- 06-advance/advance/src/concepts/03-promesas.js
+++ 06-advance/advance/src/concepts/03-promesas.js
@@ -1,4 +1,4 @@
-
+import heroes from '../data/heroes.json';
 
 /**
  * 
@@ -11,3 +11,27 @@ export const promisesComponent = (element) => {// element es el div del html
     console.log('Promises Component');
     
 }   
+
+/**
+ * 
+ * @param {String} id 
+ * @return {Promise}
+ */
+
+// funcion buscar hero q como una promesa.
+const findHero = (id) => {
+
+    // creamos la promesa con new Promise y la retornamos directamente con los parametros resolve y reject, que son funciones que se ejecutan cuando la promesa se resuelve o se rechaza
+    // el resolve se ejecuta cuando la promesa se resuelve y el reject se ejecuta cuando la promesa se rechaza
+
+    return new Promise((resolve, reject)=>{
+
+        if(hero){
+            resolve (hero);
+            return;
+        }
+        reject('Heroe con id ${id} no encontrado');
+
+  });
+   
+};
